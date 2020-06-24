5500dea3505932e78f4ca9892cec9fd4185e76e0
unknown <hugo.lach@gmail.com>
2020-06-23 23:05:03 -0400

    add columns for longer MA in current bet

--- app/Crypto/Guesser.php
+++ app/Crypto/Guesser.php
@@ -24,23 +24,38 @@ class Guesser
     $bets = [];
     $longMa1Nb = 7;
     $longMa2Nb = 22;
+    $longMaStep = 3;
+
+    $longerMa1Nb = 7;
+    $longerMa2Nb = 22;
+    $longerMaStep = 7;
 
     // Column name
     $analysis->setColumn('0diff', "Short MA Diff");
     $analysis->setColumn('1ma1', "MA 1");
     $analysis->setColumn('2ma2', "MA 2");
     $analysis->setColumn('3maPeriod', "maPeriod");
-    $analysis->setColumn('4lma1', "LONG MA 1 ($longMa1Nb)");
-    $analysis->setColumn('5lma2', "LONG MA 2 ($longMa2Nb)");
-    $analysis->setColumn('6lower', "lower");
-
+    $analysis->setColumn('4lma1', "L MA $longMaStep  ($longMa1Nb)");
+    $analysis->setColumn('5lma2', "L MA $longMaStep  ($longMa2Nb)");
+    $analysis->setColumn('6lowerlong', "lower long");
+    $analysis->setColumn('7lma1', "Lx MA $longerMaStep ($longerMa1Nb)");
+    $analysis->setColumn('8lma2', "Lx MA $longerMaStep ($longerMa2Nb)");
+    $analysis->setColumn('9lowerlong', "lower longer");
 
     foreach($tables as $table){
         $lastMAs = $analyzer->getLastMAsFromMarket($table, 7, 5);
-        $longMa1 = $analyzer->getLastMAFromMarketByStep($table, 7, 3);
-        $longMa2 = $analyzer->getLastMAFromMarketByStep($table, 22, 3);
+
+        // LONG MA
+        $longMa1 = $analyzer->getLastMAFromMarketByStep($table, $longMa1Nb, $longMaStep);
+        $longMa2 = $analyzer->getLastMAFromMarketByStep($table, $longMa2Nb, $longMaStep);
         $longMa1 = $longMa1['ma'] ?? null;
         $longMa2 = $longMa2['ma'] ?? null;
+
+        // LONGER MA
+        $longerMa1 = $analyzer->getLastMAFromMarketByStep($table, $longerMa1Nb, $longerMaStep);
+        $longerMa2 = $analyzer->getLastMAFromMarketByStep($table, $longerMa2Nb, $longerMaStep);
+        $longerMa1 = $longerMa1['ma'] ?? null;
+        $longerMa2 = $longerMa2['ma'] ?? null;
 
         // Check if moving average is always increasing
         $alwaysGoUp = true;
@@ -58,7 +73,8 @@ class Guesser
           $first = $lastMAs->first();
           $last = $lastMAs->last();
           $diff = Helpers::calcPercentageDiff($last['ma'], $first['ma']);
-          $lower = ($longMa1 < $longMa2) ? 'LOWER' : '';
+          $lower1 = ($longMa1 < $longMa2) ? 'LOWER' : '';
+          $lower2 = ($longerMa1 < $longerMa2) ? 'LOWER' : '';
           $period = sprintf("<small>%s <br> %s</small>", $last['end'], $first['end']);
 
           $analysis->setMarket($table, '0diff', $diff);
@@ -67,7 +83,10 @@ class Guesser
           $analysis->setMarket($table, '3maPeriod', $period);
           $analysis->setMarket($table, '4lma1', $longMa1);
           $analysis->setMarket($table, '5lma2', $longMa2);
-          $analysis->setMarket($table, '6lower', $lower);
+          $analysis->setMarket($table, '6lowerlong', $lower1);
+          $analysis->setMarket($table, '7lma1', $longerMa1);
+          $analysis->setMarket($table, '8lma2', $longerMa2);
+          $analysis->setMarket($table, '9lowerlong', $lower2);
 
         }
     }
